5c08ae580b59f4781c056066b156462c9ffc4088
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2024-12-15 16:32:09 +0700

    successful pop-up create dataframe

--- GettingStarted.py
+++ GettingStarted.py
@@ -1,4 +1,14 @@
 import pandas
 
+mydataset = {
+    'cars' : ["BMW", "Volvo", "Ford"],
+    'passings' : [3,7,2],
+    "Number" : ["This one", "This two", "This three"]
+}
+
+myvar = pandas.DataFrame(mydataset)
+
+print(myvar)
+
 
 
